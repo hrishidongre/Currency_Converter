3eea4faf8a876c33b9f92314f53079a21dd62a9f
hrishidongre <hrishi.dongre2024@nst.rishihood.edu.in>
2025-06-05 22:56:09 +0530

    basic prototype is done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,10 +4,15 @@ export default function Home() {
   const toCurrencyRef = useRef(null)
   const fromCurrencyRef = useRef(null)
   const [exchangeRate,setExchangeRate]  = useState(0)
+  const [currency1,setCurrency1] = useState("")
+  const [currency2,setCurrency2] = useState("")
 
   const onSubmit = useCallback(()=>{
-    const toCurrency = toCurrencyRef.current.value
     const fromCurrency  = fromCurrencyRef.current.value
+    const toCurrency = toCurrencyRef.current.value
+    setCurrency1(fromCurrencyRef.current.value)
+    setCurrency2(toCurrencyRef.current.value)
+
 
     const apiKey = process.env.NEXT_PUBLIC_CURRENCY_API_KEY;
     const apiUrl = `https://api.currencyapi.com/v3/latest?apikey=${apiKey}&currencies=${toCurrency}&base_currency=${fromCurrency}`;
@@ -16,7 +21,6 @@ export default function Home() {
        try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        // console.log("API Response:", data);
 
         setExchangeRate(data.data[toCurrency].value)
        }catch (error){
@@ -32,7 +36,8 @@ export default function Home() {
 
   // Making dropdown menu
   const [currencies,setCurrencies] = useState([])
-  const [currency,setCurrency] = useState("INR")
+  
+
 
   useEffect(()=>{
     const apiKey = process.env.NEXT_PUBLIC_CURRENCY_API_KEY
@@ -48,10 +53,6 @@ export default function Home() {
     
   },[])
 
-//   useEffect(() => {
-//   console.log(currencies, "✅ updated list of currencies");
-
-// }, [currencies]);
 
 const [amount,setAmount] = useState("")
 const [newPrice,setNewPrice] = useState("0.000")
@@ -87,9 +88,6 @@ useEffect(() => {
 
 
 
-
-  
-
   return (
     <div className="min-h-screen w-screen  bg-[linear-gradient(to_bottom,_#FFFEF6_25%,_#FFFFFF_75%)] flex flex-col items-center gap-[72px]">
 
@@ -106,17 +104,17 @@ useEffect(() => {
 
        { /*White Space*/}
       <div className="w-[892px] h-[570px] rounded-[10px] border border-black flex flex-col bg-white">
+        <h3 className="font-bold text-[15px] mt-[18px] mx-[359px]">Live Exchange Rate</h3>
 
         { /*DropDown Menu */}
 
-        <div>
+        <div className="mb-[150px]">
+          <h3 className="font-medium mx-5">From</h3>
           <select
-          className="border border-black rounded m-5 p-2 w-[200px] h-[40px]"
+          className="border border-[#DEDDDD] border-[2.5px]  rounded-[10px] mx-5 px-3 w-[200px] h-[40px] font-medium text-[17px]"
           ref = {fromCurrencyRef}
-
-          // onChange={(e) => {setCurrency(e.target.value)}}
           >
-            <option value="INR">INR</option>
+            <option value="INR" onChange={(e)=>setCurrency1(e.current.value)}>INR</option>
             {currencies.map((currency) => (
               currency!=="INR" && (<option key={currency} value={currency}>
                 {currency}
@@ -126,50 +124,54 @@ useEffect(() => {
           </select>
           <input 
             type="text"
-            className="border border-black rounded placeholder-gray-500  p-2 w-[596px] h-[40px]"
-            placeholder="fromCountry"
+            className="border border-[#DEDDDD] border-[2.5px] rounded-[10px]  placeholder-black p-2 w-[596px] h-[40px] text-[17px]"
+            placeholder="0"
             value = {amount}
             onChange={(e)=>setAmount(e.target.value)}
           />
         </div>
 
-        <div>
+        <div className="mb-[116px]">
+          <h3 className="font-medium mx-5">To</h3>
           <select
-            className="border border-black rounded m-5 p-2 w-[200px] h-[40px]"
+            className="border border-[#DEDDDD] border-[2.5px]  rounded-[10px] mx-5 px-3 w-[200px] h-[40px] font-medium text-[17px]"
             ref={toCurrencyRef}
-
-            // onChange={(e) => {setCurrency(e.target.value)}}
-          >
+            >
             <option value="USD">USD</option>
             {currencies.map((currency) => (
               currency!=="USD" && (<option key={currency} value={currency}>{currency}</option>)
             ))}
           </select>
+
           <input
             type="text"
-            className="border border-black  placeholder-gray-500 p-2 rounded w-[596px] h-[40px]"
+            className="border border-[#DEDDDD] border-[2.5px] rounded-[10px]  placeholder-black p-2 w-[596px] h-[40px] text-[17px]"
             placeholder="toCountry"
             value={newValue}
             readOnly
-          />
+            />
         </div>
 
-      <input 
-        type="submit"
-        className="border border-black w-[200px] mb-5 mx-5"
-        onClick={()=>
-        {
-          onSubmit();
-          // convertedPrice();
-        }
-        }
+        <div className="flex gap-[414px]">
 
-      />
+          <div className="rounded-[10px] w-[180px] h-[55px] bg-[#DECBA4] mx-5 flex flex-col items-center">
+            <h3 className="mt-1 text-[12px] font-medium text-[#3E5151]">Exchange Rate</h3>
+            <p className="">1 {currency1} = {exchangeRate.toFixed(3)} {currency2}</p>
+          </div>
+
+          <input 
+            type="submit"
+            value="Convert"
+            className="rounded-[10px] w-[180px] h-[55px]  mx-5 text-[20px] font-bold text-white bg-[#3E5151]"
+            onClick={()=>onSubmit()}
+            />
    
 
-      <div className="border border-black w-[287px]  p-2 mx-5">
-        <p>Rate: {exchangeRate.toFixed(3)}</p>
-      </div>
+          
+
+        </div>
+
+      
       
       
     </div>
